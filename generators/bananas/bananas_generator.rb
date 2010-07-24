7d78a8783c4599ca41d8eca3834f91d4b868a0f3
Roman Snitko <roman.snitko@gmail.com>
2010-07-24 20:14:14 +0400

    Fix: wrong file name in the generator

--- generators/bananas/bananas_generator.rb
+++ generators/bananas/bananas_generator.rb
@@ -7,7 +7,7 @@ class BananasGenerator < Rails::Generator::Base
         puts "Error: please provide report model name" 
       else
         m.migration_template 'create_bananas_reports_migration.rb.erb', "db/migrate",
-          { :assigns => { :args => bananas_options }, :migration_file_name => "create_#{bananas_options[:report_model_name].pluralize}.rb" }
+          { :assigns => { :args => bananas_options }, :migration_file_name => "create_#{bananas_options[:report_model_name].pluralize}" }
         m.template 'model.rb.erb',      "app/models/#{bananas_options[:report_model_name]}.rb", { :assigns => { :args => bananas_options } }
         m.template 'controller.rb.erb', "app/controllers/#{bananas_options[:report_model_name].pluralize}_controller.rb", { :assigns => { :args => bananas_options } }
         m.route_resources "#{bananas_options[:report_model_name].pluralize}"
