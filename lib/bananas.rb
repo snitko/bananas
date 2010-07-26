112ebb99d7f1429ccca6da69f366c261edbc52d8
Roman Snitko <subscribe@snitko.ru>
2010-07-26 15:12:11 +0400

    Fix: cast_spam_reports now accepts both abuser & abuser.id

--- lib/bananas.rb
+++ lib/bananas.rb
@@ -13,7 +13,15 @@ module Bananas
           render :file => "#{RAILS_ROOT}/public/403.html" if report_class.find_by_ip_address(request.env['REMOTE_ADDR'])
         end
         
-        self.send(:define_method, "cast_#{report_class.snake_name}") do |abuser_id|
+        self.send(:define_method, "cast_#{report_class.snake_name}") do |abuser|
+          if abuser.kind_of?(Integer)
+            abuser_id = abuser
+          elsif !abuser.nil?
+            abuser_id = abuser.id
+          else
+            abuser_id = nil
+          end
+          abuser_id = abuser if abuser.kind_of?(Integer)
           report_class.cast(:ip_address => request.env['REMOTE_ADDR'], :abuser_id => abuser_id)
         end
 
